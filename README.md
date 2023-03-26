4e1ae0366dab3a8adbbfd083376118162c30ffd7
negnq <117580177+neginashkan@users.noreply.github.com>
2023-03-26 09:17:24 +0430

    add axios,readme,update css,using axios and the meme component

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Meme Generator Website
+
+Welcome to the React Meme Generator Website! This site is built with React and allows you to easily create funny memes by adding text to images.
+
+## Features
+
+* Select an image 
+* Add text to your images
+* Share your creations with friends
+* Built with React for a fast and responsive user experience
+
+
+## Creating Memes
+
+Creating memes is easy! Simply select an image . You can then add text to the image. When you're happy with your creation, you can save it, share it.
+
+## Support
+
+If you need any help getting started, please feel free to contact us. We are always happy to help.
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,25 +1,44 @@
-import memeDataApi from "../javaScript/memesData.js";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 function Meme() {
-  const [allMemeImages, setAllMemeImages] = useState(memeDataApi.data.memes);
+  const [allMemeImagesArray, setAllMemeImagesArray] = useState([]);
+  const apiUrl = "https://api.imgflip.com/get_memes";
+
+  // ------------------------Using---axios-----------------------------
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then((response) => setAllMemeImagesArray(response.data.data.memes))
+      .catch((error) => console.log(error));
+  }, []);
+  // ------------------------Using--fetch------------------------------
+  // useEffect(() => {
+  //   async function getMemes() {
+  //     const res = await fetch(apiUrl);
+  //     const data = await res.json();
+  //     setAllMemeImagesArray(data.data.memes);
+  //   }
+  //   getMemes();
+  // }, []);
+
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
     name: "",
   });
-  function handleChange(event){
-    const {value,name}= event.target
-    setMeme(pervMeme=>({
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setMeme((pervMeme) => ({
       ...pervMeme,
-      [name]:value
+      [name]: value,
     }));
   }
   function getMemeImage(event) {
     event.preventDefault();
-    const randomIndex = Math.floor(Math.random() * allMemeImages.length),
-      url = allMemeImages[randomIndex].url,
-      name = allMemeImages[randomIndex].name;
+    const randomIndex = Math.floor(Math.random() * allMemeImagesArray.length),
+      url = allMemeImagesArray[randomIndex].url,
+      name = allMemeImagesArray[randomIndex].name;
     setMeme((pervState) => ({ ...pervState, randomImage: url, name: name }));
   }
 
